2964be87ede26f06adb59eb284c021ce5c4d4c96
Fabien Aubret <fabien.aubret@gmail.com>
2019-01-22 20:16:40 +0100

    Setting up UI

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const { app, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow } = require('electron');
 const twig = require('electron-twig');
+
+const menu_template = [
+  {
+    label: "Settings",
+    submenu: [
+      { label: "Connect",
+        click () { alert('you will connect !') }
+      }
+    ]
+  }
+];
+
+const menu = Menu.buildFromTemplate(menu_template);
+Menu.setApplicationMenu(menu);
 
 function createWindow(){
 
   win = new BrowserWindow({width: 800, height: 600});
 
-  win.loadFile('views/index.html');
+  win.loadFile('views/index.html.twig');
+
+  win.maximize()
 
   win.on('closed', () => {
     win = null;
